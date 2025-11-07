ec9f4573310167c6a728dde27abf1237bfd27dc0
Artem <artemp2208@gmail.com>
2025-11-07 18:54:12 +0200

    solution3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from typing import List, Dict
 from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: List[Dict], cafe: Cafe) -> str:
     masks_to_buy = 0
 
     for friend in friends:
